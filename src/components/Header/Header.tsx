3caceca45ab8d9a5a53933b17b8ff077a4fcd22d
benji_rc_32@hotmail.com <benjarc99@gmail.com>
2022-04-28 23:48:21 -0300

    cambio en footer y en package

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,14 @@
-
-
 const Header = () => {
-    return (
-        <div className="w-100 flex items-end justify-between p-5 bg-blue-300 border-b-4 border-blue-400 shadow">
-           <h1 className="text-4xl font-bold text-white font-serif">🌤  The Weather App</h1>
-           <small className="invisible sm:visible text-xs text-blue-600">© 2022 MH</small>
-        </div>
-    );
+  return (
+    <div className="w-100 flex items-end justify-between p-5 bg-blue-300 border-b-4 border-blue-400 shadow">
+      <h1 className="text-4xl font-bold text-white font-serif">
+        🌤 The Weather App
+      </h1>
+      <small className="invisible sm:visible text-xs text-blue-600">
+        © 2022 BQ
+      </small>
+    </div>
+  );
 };
 
 export default Header;
